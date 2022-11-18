227dca0fa66b017eb729e1c33a5c8c68d78fc10e
Mateen Mirani <mateenmirani125@gmail.com>
2022-11-18 09:46:38 +0500

    finish up the features section

--- src/components/FeatureItem.js
+++ src/components/FeatureItem.js
@@ -4,21 +4,15 @@ const FeatureItem = (props) => {
   const { img, title, description, linkText } = props;
   return (
     <>
-      <div className="w-10/13 mx-auto">
-        <div className="grid-cols-1">
-          <div className="flex flex-col">
-            <img src={img} alt="FeatureImg" />
-            <h1>{title}</h1>
-            <p>{description}</p>
-            <div className="pt-4">
-              <span className=" text-[#3468BC] cursor-pointer text-md hover:mr-1  font-semibold hover:underline lg:text-black ">
-                {linkText}
-              </span>
-              <span className="ml-1 text-2xl text-[#3468BC] lg:text-black">
-                &gt;
-              </span>
-            </div>
-          </div>
+      <div className="flex flex-col">
+        <img src={img} alt="FeatureImg" className="w-full" />
+        <h1 className="text-xl font-semibold py-3 lin">{title}</h1>
+        <p className="">{description}</p>
+        <div className="py-4">
+          <span className=" text-[#3468BC] cursor-pointer text-md hover:mr-1  font-semibold hover:underline  ">
+            {linkText}
+          </span>
+          <span className="ml-1 text-2xl text-[#3468BC]">&gt;</span>
         </div>
       </div>
     </>
--- src/components/Features.js
+++ src/components/Features.js
@@ -1,51 +1,56 @@
 import React from "react";
 import FeatureItem from "./FeatureItem";
-import g1 from "../assets/designed.jpg"
+import designImg from "../assets/designed.jpg";
+import edgeImg from "../assets/Edge.webp";
+import oneDriveImg from "../assets/oneDrive.webp";
+import oneNoteImg from "../assets/note.webp";
 
 const Features = () => {
   const dummy_data = [
     {
-      img: g1,
+      img: designImg,
       title: "Designed for life today – and tomorrow",
       description:
         "The next-generation of games. Your goals. Friends and family. Windows 11 was made to bring you closer to everything you love.",
       linkText: "See if your PC is ready",
     },
     {
-      img: "../assets/Edge.webp",
+      img: edgeImg,
       title: "Microsoft Edge",
       description:
         "World-class performance with more privacy, more productivity and more value while you browse.",
       linkText: "Download now",
     },
     {
-      img: "../assets/oneDrive.webp",
+      img: oneDriveImg,
       title: "Microsoft OneDrive",
       description:
         "Save your files and photos to OneDrive and access them from any device, anywhere.",
       linkText: "Learn more",
     },
     {
-      img: "../assets/note.webp",
+      img: oneNoteImg,
       title: "OneNote",
       description: "Organise your note and your life.",
       linkText: "Learn more",
     },
   ];
   return (
-    <>
-      {dummy_data.map((item, index) => {
-        return (
-          <FeatureItem
-            key={index}
-            img={item.img}
-            title={item.title}
-            description={item.description}
-            linkText={item.linkText}
-          />
-        );
-      })}
-    </>
+    <div className="w-11/13 mx-auto lg:w-10/13">
+      <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:my-20 lg:grid-cols-4">
+        {dummy_data.map((item, index) => {
+          return (
+            <FeatureItem
+              key={index}
+              img={item.img}
+              title={item.title}
+              description={item.description}
+              linkText={item.linkText}
+            />
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
